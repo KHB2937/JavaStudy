5ba0696c4a74b88363cfe6db52dc189e8ae55221
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-04-17 03:47:30 +0000

    Hello Python!

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ class Main {
     System.out.println("Hello World!");
     System.out.println("Hello Playdata!");
     System.out.println("Hello Java!");
+    System.out.println("Hello Python!");
   }
 }
