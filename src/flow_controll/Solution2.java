df85ddc95ef7337f80f48c0788f228f0ff0f9c30
qus0in <qus0in@gmail.com>
2023-04-21 12:52:05 +0900

    ~ for. for each.

---
+++ src/flow_controll/Solution2.java
@@ -0,0 +1,46 @@
+package flow_controll;
+// 같은 패키지 경로 안에 있으면 한 파일에 있는 듯이 해당 enum, class 등을 사용 가능.
+
+public class Solution2 {
+    public static void main(String[] args) {
+        // Switch-case
+        // switch -> 특정 값인지 여부를 검사할 변수명을 넣음 (switch(...))
+        // -> 특정한 값을 만족시키는지를 case 값: ...
+        // example code please
+//        int a = 10;
+        // int a가 0부터 30 사이의 값을 랜덤으로 할당받음
+        int a = (int) (Math.random() * 31);
+        System.out.println("a = " + a);
+        switch (a) {
+            case 10:
+                System.out.println("10");
+                break;
+            case 20:
+                System.out.println("20");
+                break;
+            case 30:
+                System.out.println("30");
+                break;
+            default:
+                System.out.println("default");
+        }
+
+        // Chicken enum을 사용하여, 전체 속성 중에 하나를 랜덤으로 뽑아 변수로 대입하고,
+        // 그 변수에 따라서 switch case 문으로 각기 다른 메시지 출력
+        Chicken chicken = Chicken.values()[(int) (Math.random() * Chicken.values().length)];
+        System.out.println("chicken = " + chicken);
+        switch (chicken) {
+            case Fried:
+                System.out.println("Fried");
+                break;
+            case GreenOnion:
+                System.out.println("GreenOnion");
+                break;
+            case HoneyCombo:
+                System.out.println("HoneyCombo");
+                break;
+            default:
+                System.out.println("default");
+        } // java: reached end of file while parsing
+    }
+}
---
+++ src/flow_controll/Solution3.java
@@ -0,0 +1,71 @@
+package flow_controll;
+
+public class Solution3 {
+    public static void main(String[] args) {
+        // 반복문 - while문.
+        int i = 0;
+        while (i < 10) { // while(조건) {실행문} -> 조건이 True 될때까지 실행
+            System.out.println(i);
+            i++; // 증가연산자. i = i + 1
+        }
+
+//        int i2 = 0;
+//        while (i2 < 10) { // while(조건) {실행문} -> 조건이 True 될때까지 실행
+//            System.out.println(i2); // 중단 조건이 없기 때문에 계속 공급
+//        }
+
+        // python while 반복문
+        // break & continue
+//        int i10 = 0;
+//        while (i10 < 10) { // while(조건) {실행문} -> 조건이 True 될때까지 실행
+//            System.out.println(i10);
+//        }
+
+        int i3 = 1;
+        while (i3 < 10) { // while(조건) {실행문} -> 조건이 True 될때까지 실행
+            System.out.println("Hello i3 = " + i3);
+            // 3의 배수 발견시 while 강제 종료
+            if (i3 % 3 == 0) {
+                System.out.println("Bye i3 = " + i3);
+                break;
+            }
+            i3++;
+        }
+
+        // continue
+        int i4 = 0;
+        while (i4 < 10) { // while(i4 < 10) {
+            i4++;
+            if (i4 % 2 == 0) { // i4가 2의 배수라면, 반복을 스킵하고 다음으로 넘어감
+                continue; // 더이상 뒤를 실행하지 않고, 다음 반복으로.
+            }
+            System.out.println("i4 = " + i4);
+        }
+
+        // 항상 참인 while (무한 루프)
+        int i5 = 1;
+        while (true) { // while(true) {실행문} -> 중간에 break가 나올 때까지 반복 실행
+            // 횟수나 조건이 가변적일 수 있는 상황에서, while 내부 코드로 break를 통해 반복 제어
+            System.out.println("Hello i5 = " + i5);
+            // 3의 배수 발견시 while 강제 종료
+            if (i5 % 3 == 0) {
+                System.out.println("Bye i5 = " + i5);
+                break;
+            }
+            i5++;
+        }
+
+        // do while
+        int i6 = 10;
+        do { // 한 번은 함
+            System.out.println("Hello i6 = " + i6);
+            i6++;
+        } while (i6 < 10);
+        // while (실행이 안되요)
+        int i7 = 10;
+        while (i7 < 10) { // if 조건문 내장
+            System.out.println("Hello i7 = " + i7);
+            i7++;
+        }
+    }
+}
