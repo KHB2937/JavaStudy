5eb71a9198b7577220afb8006a3bb17b8b4dc212
qus0in <qus0in@gmail.com>
2023-05-02 03:27:36 +0900

    thymeleaf

--- src/main/java/io/playdata/webapp/HelloWorldController.java
+++ src/main/java/io/playdata/webapp/HelloWorldController.java
@@ -1,17 +1,16 @@
 package io.playdata.webapp;
-import org.springframework.stereotype.Controller; // 스프링에서 컨트롤러임을 나타내는 어노테이션을 import
-import org.springframework.web.bind.annotation.RequestMapping; // 요청 매핑 어노테이션을 import
-import org.springframework.web.servlet.ModelAndView; // 모델 데이터와 뷰 이름을 담는 객체를 import
 
-@Controller // 컨트롤러 역할을 하는 클래스에 사용하는 어노테이션
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // 이 클래스가 스프링에서 컨트롤러 역할을 수행하는 클래스임을 나타내는 애노테이션입니다.
+
 public class HelloWorldController {
 
-    @RequestMapping("/") // URL 경로를 매핑하는 어노테이션, 현재는 "/" 경로에 대한 요청을 처리
-    public ModelAndView hello() { // 요청에 대한 응답을 처리하는 메소드
-        System.out.println("hello");
-        ModelAndView mv = new ModelAndView(); // ModelAndView 객체 생성
-        mv.setViewName("index"); // 뷰 이름 설정, 여기서는 index.jsp 파일과 연결
-        mv.addObject("message", "Hello World!"); // 모델 데이터 설정, key-value 형태로 전달
-        return mv; // ModelAndView 객체 반환
+    @GetMapping("/") // HTTP GET 요청이 / 경로로 들어오면 실행되는 메서드를 정의한 것입니다.
+    public String hello(Model model) { // Model 객체를 인자로 받아서 뷰에 데이터를 전달할 수 있습니다.
+        model.addAttribute("name", "World"); // "name"이라는 이름으로 "World"라는 값을 Model 객체에 추가합니다.
+        return "hello"; // "hello"라는 뷰 이름을 반환합니다. 이는 스프링에서 뷰 리졸버가 "hello"라는 이름의 뷰를 찾아 클라이언트에게 전달합니다.
     }
 }
--- src/main/webapp/index.jsp
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html> <!-- HTML5 문서 유형 선언 -->
-<html>
-<head>
-    <title>Hello World</title> <!-- 문서 제목 설정 -->
-</head>
-<body>
-    <h1>${message}</h1> <!-- ${message}는 컨트롤러에서 전달한 "Hello World!" 메시지를 출력하는 부분 -->
-</body>
-</html>
